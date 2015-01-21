df385de003dc81d2a25401739b0b65bf71ad47e6
Reid Hochstedler <rhochstedler@lafayetteinstrument.com>
2015-01-21 15:47:19 -0500

    Initial mutation classes.

---
+++ Scopes.Engine/Mutation/IMutation.cs
@@ -0,0 +1,21 @@
+﻿namespace Scopes.Engine.Mutation
+{
+    using System.Diagnostics.Contracts;
+
+    [ContractClass(typeof(MutationContract))]
+    public interface IMutation
+    {
+        Chromosome Mutate(Chromosome original);
+    }
+
+    [ContractClassFor(typeof(IMutation))]
+    internal abstract class MutationContract : IMutation
+    {
+        public Chromosome Mutate(Chromosome original)
+        {
+            Contract.Requires(original != null);
+            Contract.Ensures(Contract.Result<Chromosome>() != null);
+            return default(Chromosome);
+        }
+    }
+}
---
+++ Scopes.Engine/Mutation/SinglePointMutation.cs
@@ -0,0 +1,41 @@
+﻿namespace Scopes.Engine.Mutation
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using MathNet.Numerics.Random;
+
+    using Scopes.Engine.Nodes;
+
+    public class SinglePointMutation : IMutation
+    {
+        private readonly MersenneTwister random = MersenneTwister.Default;
+
+        public Chromosome Mutate(Chromosome original)
+        {
+            var nodes = original.Nodes;
+            var functionSet = original.FunctionSet;
+            var terminalSet = original.TerminalSet;
+            var newNodes = new List<IGepNode>(nodes);
+            var index = random.Next(0, nodes.Count);
+            if (index > original.HeadLength) {
+                // Terminal node only.
+                var setLength = terminalSet.Count;
+                newNodes[index] = terminalSet.ElementAt(random.Next(0, setLength - 1))();
+            } else {
+                // Function or Terminal node.  
+                var isFunction = this.random.NextDouble() > 0.5d;
+                if (isFunction)
+                {
+                    var setLength = functionSet.Count;
+                    newNodes[index] = functionSet.ElementAt(random.Next(0, setLength - 1))();
+                } else {
+                    var setLength = terminalSet.Count;
+                    newNodes[index] = terminalSet.ElementAt(random.Next(0, setLength - 1))();
+                }
+            }
+
+            return new Chromosome(original.HeadLength, original.NumGenes, original.FunctionSet, original.TerminalSet, newNodes);
+        }
+    }
+}
