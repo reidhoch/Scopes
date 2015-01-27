6bf70d63e22673fa9f423fc9d75d4ff0c4c7da11
Reid Hochstedler <rhochstedler@lafayetteinstrument.com>
2015-01-27 16:10:41 -0500

    Added DummyChromosome

---
+++ Scopes.Engine.Tests/DummyChromosome.cs
@@ -0,0 +1,33 @@
+﻿namespace Scopes.Engine.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    using Scopes.Engine.Nodes;
+
+    public class DummyChromosome : Chromosome
+    {
+        private static readonly ISet<Func<IFunctionNode>> DummyFunctionSet = new HashSet<Func<IFunctionNode>>
+                                  {
+                                      () => new SquareRootNode(),
+                                      () => new AdditionNode(),
+                                      () => new SubtractionNode(),
+                                      () => new MultiplicationNode(),
+                                      () => new DivisionNode()
+                                  };
+        private static readonly ISet<Func<ITerminalNode>> DummyTerminalSet = new HashSet<Func<ITerminalNode>>
+                                  {
+                                      () => new ConstantNode(),
+                                      () => new VariableNode()
+                                  };
+        private static int fitness;
+
+        public DummyChromosome()
+            : base(10, 1, DummyFunctionSet, DummyTerminalSet)
+        {
+            this.Fitness = fitness;
+            Interlocked.Increment(ref fitness);
+        }
+    }
+}
