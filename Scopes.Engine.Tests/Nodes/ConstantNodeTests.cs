32328bdbadaa1806cb93275882c77f8747b2cfa3
Reid Hochstedler <rhochstedler@lafayetteinstrument.com>
2015-02-06 16:33:03 -0500

    ConstantNode tests.

--- Scopes.Engine.Tests/Nodes/ConstantNodeTests.cs
+++ Scopes.Engine.Tests/Nodes/ConstantNodeTests.cs
@@ -23,11 +23,71 @@
         }
 
         [Test]
+        public void Clone([Random(1.0, 10.0, 5)]double value)
+        {
+            var node = new ConstantNode { Value = value };
+            Assert.That(node.Clone(), Is.Not.Null);
+        }
+
+        [Test]
+        public void Equals([Random(1.0, 10.0, 5)]double value)
+        {
+            var left = new ConstantNode { Value = value };
+            var right = new ConstantNode { Value = value };
+            Assert.That(left.Equals(right), Is.True);
+        }
+
+        [Test]
+        public void EqualsObject([Random(1.0, 10.0, 5)]double value)
+        {
+            var left = new ConstantNode { Value = value };
+            var right = new ConstantNode { Value = value };
+            Assert.That(left.Equals((object)right), Is.True);
+        }
+
+        [Test]
+        public void EqualsNullIsFalse([Random(1.0, 10.0, 5)]double value)
+        {
+            var left = new ConstantNode { Value = value };
+            Assert.That(left.Equals(null), Is.False);
+        }
+
+        [Test]
+        public void DifferentTypesAreNotEqual([Random(1.0, 10.0, 5)]double value)
+        {
+            var left = new ConstantNode { Value = value };
+            var right = new SubtractionNode
+            {
+                Children =
+                                   {
+                                       new ConstantNode { Value = value },
+                                       new ConstantNode { Value = 2.0d * value }
+                                   }
+            };
+            Assert.That(left.Equals(right), Is.False);
+        }
+
+        [Test]
+        public void EqualsNullObjectIsFalse([Random(1.0, 10.0, 5)]double value)
+        {
+            var left = new ConstantNode { Value = value };
+            Assert.That(left.Equals((object)null), Is.False);
+        }
+
+        [Test]
         public void Evaluate([Random(1.0, 10.0, 5)]double value)
         {
             var node = new ConstantNode { Value = value };
 
             Assert.That(node.Evaluate(new double[0]), Is.EqualTo(value));
+        }
+
+        [Test]
+        public void GetHashCode([Random(1.0, 10.0, 5)]double value)
+        {
+            var node = new ConstantNode { Value = value };
+            var expected = value.GetHashCode();
+            Assert.That(node.GetHashCode(), Is.EqualTo(expected));
         }
     }
 }
