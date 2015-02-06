2e4d8e58dff777ad51c605f5aa902ad14f780edd
Reid Hochstedler <rhochstedler@lafayetteinstrument.com>
2015-02-06 16:38:53 -0500

    VariableNode tests.

--- Scopes.Engine.Tests/Nodes/VariableNodeTests.cs
+++ Scopes.Engine.Tests/Nodes/VariableNodeTests.cs
@@ -22,12 +22,72 @@
         }
 
         [Test]
+        public void Clone([Random(1, 10, 5)]int value)
+        {
+            var node = new VariableNode(value);
+            Assert.That(node.Clone(), Is.Not.Null);
+        }
+
+        [Test]
+        public void Equals([Random(1, 10, 5)]int value)
+        {
+            var left = new VariableNode(value);
+            var right = new VariableNode(value);
+            Assert.That(left.Equals(right), Is.True);
+        }
+
+        [Test]
+        public void EqualsObject([Random(1, 10, 5)]int value)
+        {
+            var left = new VariableNode(value);
+            var right = new VariableNode(value);
+            Assert.That(left.Equals((object)right), Is.True);
+        }
+
+        [Test]
+        public void EqualsNullIsFalse([Random(1, 10, 5)]int value)
+        {
+            var left = new VariableNode(value);
+            Assert.That(left.Equals(null), Is.False);
+        }
+
+        [Test]
+        public void DifferentTypesAreNotEqual([Random(1, 10, 5)]int value)
+        {
+            var left = new VariableNode(value);
+            var right = new SubtractionNode
+            {
+                Children =
+                                   {
+                                       new ConstantNode { Value = value },
+                                       new ConstantNode { Value = 2.0d * value }
+                                   }
+            };
+            Assert.That(left.Equals(right), Is.False);
+        }
+
+        [Test]
+        public void EqualsNullObjectIsFalse([Random(1, 10, 5)]int value)
+        {
+            var left = new VariableNode(value);
+            Assert.That(left.Equals((object)null), Is.False);
+        }
+
+        [Test]
         public void Evaluate([Random(0.0, 10.0, 5)]double value)
         {
             var parameters = new[] { value };
             var node = new VariableNode(0);
 
             Assert.That(node.Evaluate(parameters), Is.EqualTo(value));
+        }
+
+        [Test]
+        public void GetHashCode([Random(1, 10, 5)]int index)
+        {
+            var node = new VariableNode(index);
+            var expected = index.GetHashCode();
+            Assert.That(node.GetHashCode(), Is.EqualTo(expected));
         }
     }
 }
