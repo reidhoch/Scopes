4e13a7d4ad7390160b420787431fce4fc71f2d3e
Reid Hochstedler <rhochstedler@lafayetteinstrument.com>
2015-02-06 16:03:20 -0500

    AdditionNode tests.

--- Scopes.Engine.Tests/Nodes/AdditionNodeTests.cs
+++ Scopes.Engine.Tests/Nodes/AdditionNodeTests.cs
@@ -23,6 +23,114 @@
         }
 
         [Test]
+        public void Clone([Random(5)]double left, [Random(5)]double right)
+        {
+            var node = new AdditionNode
+                           {
+                               Children =
+                                   {
+                                       new ConstantNode { Value = left },
+                                       new ConstantNode { Value = right }
+                                   }
+                           };
+            Assert.That(node.Clone(), Is.Not.Null);
+        }
+
+        [Test]
+        public void Equals([Random(5)]double leftVal, [Random(5)]double rightVal)
+        {
+            var left = new AdditionNode
+            {
+                Children =
+                                   {
+                                       new ConstantNode { Value = leftVal },
+                                       new ConstantNode { Value = rightVal }
+                                   }
+            };
+            var right = new AdditionNode
+            {
+                Children =
+                                   {
+                                       new ConstantNode { Value = leftVal },
+                                       new ConstantNode { Value = rightVal }
+                                   }
+            };
+            Assert.That(left.Equals(right), Is.True);
+        }
+
+        [Test]
+        public void EqualsObject([Random(5)]double leftVal, [Random(5)]double rightVal)
+        {
+            var left = new AdditionNode
+            {
+                Children =
+                                   {
+                                       new ConstantNode { Value = leftVal },
+                                       new ConstantNode { Value = rightVal }
+                                   }
+            };
+            var right = new AdditionNode
+            {
+                Children =
+                                   {
+                                       new ConstantNode { Value = leftVal },
+                                       new ConstantNode { Value = rightVal }
+                                   }
+            };
+            Assert.That(left.Equals((object)right), Is.True);
+        }
+
+        [Test]
+        public void EqualsNullIsFalse([Random(5)]double leftVal, [Random(5)]double rightVal)
+        {
+            var left = new AdditionNode
+            {
+                Children =
+                                   {
+                                       new ConstantNode { Value = leftVal },
+                                       new ConstantNode { Value = rightVal }
+                                   }
+            };
+            Assert.That(left.Equals(null), Is.False);
+        }
+
+        [Test]
+        public void DifferentTypesAreNotEqual([Random(5)]double leftVal, [Random(5)]double rightVal)
+        {
+            var left = new AdditionNode
+            {
+                Children =
+                                   {
+                                       new ConstantNode { Value = leftVal },
+                                       new ConstantNode { Value = rightVal }
+                                   }
+            };
+            var right = new SubtractionNode
+            {
+                Children =
+                                   {
+                                       new ConstantNode { Value = leftVal },
+                                       new ConstantNode { Value = rightVal }
+                                   }
+            };
+            Assert.That(left.Equals(right), Is.False);
+        }
+
+        [Test]
+        public void EqualsNullObjectIsFalse([Random(5)]double leftVal, [Random(5)]double rightVal)
+        {
+            var left = new AdditionNode
+            {
+                Children =
+                                   {
+                                       new ConstantNode { Value = leftVal },
+                                       new ConstantNode { Value = rightVal }
+                                   }
+            };
+            Assert.That(left.Equals((object)null), Is.False);
+        }
+
+        [Test]
         public void Evaluate([Random(1.0, 10.0, 5)]double leftVal, [Random(1.0, 10.0, 5)]double rightVal)
         {
             var expected = leftVal + rightVal;
@@ -31,6 +139,21 @@
             var node = new AdditionNode { Children = { left, right } };
 
             Assert.That(node.Evaluate(new double[0]), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void GetHashCode([Random(5)] double leftVal, [Random(5)] double rightVal)
+        {
+            var node = new AdditionNode
+                           {
+                               Children =
+                                   {
+                                       new ConstantNode { Value = leftVal },
+                                       new ConstantNode { Value = rightVal }
+                                   }
+                           };
+            var expected = node.Children.GetHashCode();
+            Assert.That(node.GetHashCode(), Is.EqualTo(expected));
         }
     }
 }
