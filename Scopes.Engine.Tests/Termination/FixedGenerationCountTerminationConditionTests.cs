8602d31cf9938b5dc46ac57056bb90fd81c10869
Reid Hochstedler <reidhoch@gmail.com>
2015-02-08 16:04:47 -0500

    Added FixedGenerationCountTerminationCondition

---
+++ Scopes.Engine.Tests/Termination/FixedGenerationCountTerminationConditionTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Scopes.Engine.Tests.Termination
+{
+    using NUnit.Framework;
+    using Scopes.Engine.Termination;
+
+    [TestFixture]
+    public class FixedGenerationCountTerminationConditionTests
+    {
+        [Test]
+        public void IsSatisfied([Random(1, 500, 10)]int generations)
+        {
+            var fgc = new FixedGenerationCountTerminationCondition(generations);
+            var count = 0;
+            while (!fgc.IsSatisfied()) {
+                count++;
+            }
+            Assert.That(count, Is.EqualTo(generations));
+        }
+    }
+}
---
+++ Scopes.Engine/Termination/FixedGenerationCountTerminationCondition.cs
@@ -0,0 +1,27 @@
+﻿namespace Scopes.Engine.Termination
+{
+    using System;
+    using System.Diagnostics.Contracts;
+    using System.Threading;
+
+    public class FixedGenerationCountTerminationCondition : ITerminationCondition
+    {
+        private readonly int numGenerations;
+        private int generation = 0;
+
+        public FixedGenerationCountTerminationCondition(int numGenerations)
+        {
+            Contract.Requires<ArgumentOutOfRangeException>(numGenerations > 0);
+            this.numGenerations = numGenerations;
+        }
+
+        public bool IsSatisfied()
+        {
+            if (this.generation < this.numGenerations) {
+                Interlocked.Increment(ref generation);
+                return false;
+            }
+            return true;
+        }
+    }
+}
