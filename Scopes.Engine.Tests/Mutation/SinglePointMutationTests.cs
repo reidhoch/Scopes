06e84d6ce252a9c62290454841593c64aaf2137a
Reid Hochstedler <rhochstedler@lafayetteinstrument.com>
2015-02-13 15:50:36 -0500

    Multigenic mutation.

--- Scopes.Engine.Tests/Mutation/SinglePointMutationTests.cs
+++ Scopes.Engine.Tests/Mutation/SinglePointMutationTests.cs
@@ -42,5 +42,30 @@
 
             Assert.That(difference, Is.EqualTo(1));
         }
+
+        [Test]
+        public void MutateMultigenic([Values(1.0, 2.0)]double a, [Values(1.0, 2.0)]double b, [Values(1.0, 2.0)]double c)
+        {
+            var aNode = new ConstantNode { Value = a };
+            var bNode = new ConstantNode { Value = b };
+            var cNode = new ConstantNode { Value = c };
+            var add = new AdditionNode();
+            var subtract = new SubtractionNode();
+            var mutation = new SinglePointMutation();
+            var nodes = new List<IGepNode> { add, subtract, aNode, bNode, cNode, add, subtract, aNode, bNode, cNode };
+            var original = new Chromosome(2, 2, 3, FunctionSet, nodes) { LinkingFunction = new AdditionNode() };
+            var mutant = mutation.Mutate(original);
+
+            var difference = 0;
+            for (var idx = 0; idx < 10; idx++)
+            {
+                if (!mutant.Nodes[idx].Equals(original.Nodes[idx]))
+                {
+                    difference++;
+                }
+            }
+
+            Assert.That(difference, Is.EqualTo(1));
+        }
     }
 }
--- Scopes.Engine/Mutation/SinglePointMutation.cs
+++ Scopes.Engine/Mutation/SinglePointMutation.cs
@@ -1,5 +1,6 @@
 ﻿namespace Scopes.Engine.Mutation
 {
+    using System;
     using System.Collections.Generic;
     using System.Diagnostics.Contracts;
     using System.Linq;
@@ -22,22 +23,41 @@
             var linkingFunction = original.LinkingFunction;
             var newNodes = new List<IGepNode>();
             newNodes.AddRange(nodes.Select(node => node.Clone()));
-            var index = random.Next(0, nodes.Count);
-            if (index > (original.HeadLength - 1)) {
+            var mutationPoint = random.Next(0, nodes.Count);
+            if (!IsHead(mutationPoint, original.NumGenes, original.HeadLength, original.TailLength)) {
                 // Terminal node only.
-                newNodes[index] = terminalFactory.Generate(parameterCount, 1, 10);
+                newNodes[mutationPoint] = terminalFactory.Generate(parameterCount, 1, 10);
             } else {
                 // Function or Terminal node.  
                 var isFunction = this.random.NextDouble() > 0.5d;
                 if (isFunction) {
                     var setLength = functionSet.Count;
-                    newNodes[index] = functionSet.ElementAt(random.Next(0, setLength - 1))();
+                    newNodes[mutationPoint] = functionSet.ElementAt(random.Next(0, setLength - 1))();
                 } else {
-                    newNodes[index] = terminalFactory.Generate(parameterCount, 1, 10);
+                    newNodes[mutationPoint] = terminalFactory.Generate(parameterCount, 1, 10);
                 }
             }
 
             return new Chromosome(original.HeadLength, original.NumGenes, original.ParameterCount, original.FunctionSet, newNodes) { LinkingFunction = linkingFunction };
+        }
+
+        private static bool IsHead(int mutationPoint, int numGenes, int headLength, int tailLength)
+        {
+            Contract.Requires<ArgumentOutOfRangeException>(numGenes > 0);
+            var start = new int[numGenes];
+            var end = new int[numGenes];
+            start[0] = 0;
+            end[0] = headLength;
+            for (var i = 1; i < numGenes; i++) {
+                start[i] = i * (headLength + tailLength);
+                end[i] = start[i] + headLength;
+            }
+            for (var j = 0; j < numGenes; j++)
+            {
+                if (mutationPoint >= start[j] && mutationPoint < end[j]) return true;
+            }
+
+            return false;
         }
 
         [ContractInvariantMethod]
